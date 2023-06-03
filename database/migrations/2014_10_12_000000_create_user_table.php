2e3e6554ba83d27d64cfdbd2689808bbf4dd790e
gbrn7 <mraygbrn@gmail.com>
2023-06-03 13:31:48 +0700

    update customer role

--- database/migrations/2014_10_12_000000_create_user_table.php
+++ database/migrations/2014_10_12_000000_create_user_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('user', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->enum('role', ['admin', 'member']);
+            $table->enum('role', ['admin', 'customer']);
             $table->string('email')->unique();
             $table->string('password')->nullable();
             $table->string('alamat')->nullable();
